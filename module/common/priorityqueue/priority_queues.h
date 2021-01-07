fc9d6c88883adb33441e2b1c7366e26296cc489d
qbb <qbb@LAPTOP-6A0LGM1A>
2021-01-08 00:24:07 +0800

    set module priority queue interface

--- module/common/priorityqueue/priority_queues.h
+++ module/common/priorityqueue/priority_queues.h
@@ -10,8 +10,44 @@
 #include "../x_cross_platform.h"
 #include "../CommonMarco.h"
 
+#define INVALID_PRIOQUEUE_PRIORITY  (0xFFFF) 
+#define CURRENT_PRIORITY uint16_t
+
+
+#define APP_BIT_TABLE_PRIORITYQUEUE_DEF_WITHOUT_POINTER() 
+#define APP_BIT_TABLE_PRIORITYQUEUE_DEF() 
+
+
+X_Void 				BT_PriorityQueueInit(const sPrioListManager *p_manager);
+CURRENT_PRIORITY 	BT_PriorityQueueInsert(const sPrioListManager *p_manager,uint16_t prio_to_insert);
+CURRENT_PRIORITY 	BT_PriorityQueueFindMin(const sPrioListManager *p_manager);
+CURRENT_PRIORITY 	BT_PriorityQueueReleaseMin(const sPrioListManager *p_manager);
+X_Void 				BT_PriorityQueueClear(const sPrioListManager *p_manager);
+X_Boolean 			BT_DoesPriorityQueueEmpty(const sPrioListManager *p_manager);
+uint16_t 			BT_GetPriorityQueueUsedNodeNum(const sPrioListManager *p_manager);
+
+
 /**********************
 *************************/
+
+struct X_BinaryHeapStruct;
+typedef struct X_BinaryHeapStruct *X_PriorityQueue;
+
+X_PriorityQueue 		BH_PriorityQueueInit(uint16_t max_elements);
+X_Void 					BH_PriorityQueueDestory(X_PriorityQueue H);
+X_Void 					BH_PriorityQueueClear(X_PriorityQueue H);
+CURRENT_PRIORITY 		BH_PriorityQueueInsert(X_PriorityQueue H,uint16_t priority);
+CURRENT_PRIORITY 		BH_PriorityQueueFindMin(X_PriorityQueue H);
+CURRENT_PRIORITY 		BH_PriorityQueueReleaseMin(X_PriorityQueue H);
+X_boolean 				BH_DoesPriorityQueueEmpty(X_PriorityQueue H);
+uint16_t 				BH_GetPriorityQueueUsedNodeNum(X_PriorityQueue H);
+
+
+
+
+
+
+
 
 #ifdef __cplusplus
 }
--- module/common/priorityqueue/test/priorityqueue_test.cc
+++ module/common/priorityqueue/test/priorityqueue_test.cc
@@ -1,22 +1,44 @@
 #include <iostream>
 #include "tdd_common.h"
 
-#include "../loop_queues.h"
+#include "../priority_queues.h"
 
 using namespace std;
 
+/***********************
+X_Void 				BT_PriorityQueueInit(const sPrioListManager *p_manager);
+CURRENT_PRIORITY 	BT_PriorityQueueInsert(const sPrioListManager *p_manager,uint16_t prio_to_insert);
+CURRENT_PRIORITY 	BT_PriorityQueueFindMin(const sPrioListManager *p_manager);
+CURRENT_PRIORITY 	BT_PriorityQueueReleaseMin(const sPrioListManager *p_manager);
+X_Void 				BT_PriorityQueueClear(const sPrioListManager *p_manager);
+X_Boolean 			BT_DoesPriorityQueueEmpty(const sPrioListManager *p_manager);
+uint16_t 			BT_GetPriorityQueueUsedNodeNum(const sPrioListManager *p_manager);
+
+*************************/
+/************************
+X_PriorityQueue 		BH_PriorityQueueInit(uint16_t max_elements);
+X_Void 					BH_PriorityQueueDestory(X_PriorityQueue H);
+X_Void 					BH_PriorityQueueClear(X_PriorityQueue H);
+CURRENT_PRIORITY 		BH_PriorityQueueInsert(X_PriorityQueue H,uint16_t priority);
+CURRENT_PRIORITY 		BH_PriorityQueueFindMin(X_PriorityQueue H);
+CURRENT_PRIORITY 		BH_PriorityQueueReleaseMin(X_PriorityQueue H);
+X_boolean 				BH_DoesPriorityQueueEmpty(X_PriorityQueue H);
+uint16_t 				BH_GetPriorityQueueUsedNodeNum(X_PriorityQueue H);
+
+*************************/
+
 static  uint16_t buf_number;
-SIMPLE_LOOPQUEUE_DEF(p_queue,200);
 #define OPERATION_TIMES 0xfffe
+APP_PRIORITYQUEUE_DEF_WITHOUT_POINTER(p_prio_queue,5000);
 
 TEST(queue_test,normal)
 {
 	uint16_t i = 200;
 	static 	uint8_t data_buf[200];
-	SimpleQueueInitialize(p_queue);
+	PriorityQueueInit(p_prio_queue);
 
 	do{
-		buf_number = SimpleQueueFirstIn(p_queue,X_False);
+		buf_number = PriorityQueuePush(p_queue,X_False);
 		EXPECT_LT(buf_number,200);
 		
 		data_buf[buf_number] = i;
@@ -30,127 +52,6 @@ TEST(queue_test,normal)
 	}while(i-- > 1);
 	
 };
-
-
-TEST(queue_test,big_amount_push_pop)
-{
-	uint16_t i = OPERATION_TIMES;
-	SimpleQueueInitialize(p_queue);
-
-
-	do{
-		buf_number = SimpleQueueFirstIn(p_queue,X_True);
-		EXPECT_LT(buf_number,OPERATION_TIMES);
-		
-		buf_number = SimpleQueueFirstOut(p_queue);
-		EXPECT_EQ((OPERATION_TIMES-i)%200,buf_number);
-		
-	}while(i-- > 1);
-	
-};
-
-
-
-TEST(queue_test,node_occupy_test)
-{
-	SIMPLE_LOOPQUEUE_DEF(p_big_queue,10000);
-	uint16_t i = 0;
-	
-	SimpleQueueInitialize(p_big_queue);
-
-	do{
-		buf_number = SimpleQueueFirstIn(p_big_queue,X_False);
-		if(i < 10000)
-		{
-			EXPECT_EQ(buf_number,i);
-		}
-		else
-		{
-			EXPECT_EQ(buf_number,INVALID_NODE_NUM);
-		}
-		
-	}while((i++) < (OPERATION_TIMES - 1));
-	
-	EXPECT_EQ(10000,GetSimpleQueueUsedNodeNumber(p_big_queue));
-	
-	i = 0;
-	do{
-		buf_number = SimpleQueueFirstOut(p_big_queue);
-		EXPECT_EQ(i,buf_number);
-		
-	}while(i++ < 9999);
-	
-};
-
-#define BIG_QUEUE_SIZE (10000)
-TEST(queue_test,time_test)
-{
-	uint16_t i = OPERATION_TIMES,j;
-	SIMPLE_LOOPQUEUE_DEF(p_big_queue,BIG_QUEUE_SIZE);
-	
-	for(j=0;j<10;j++)
-	{
-		do{
-		buf_number = SimpleQueueFirstIn(p_big_queue,X_True);
-		EXPECT_LT(buf_number,OPERATION_TIMES);
-		
-		buf_number = SimpleQueueFirstOut(p_big_queue);
-		EXPECT_EQ((OPERATION_TIMES-i)%BIG_QUEUE_SIZE,buf_number);
-		
-		}while(i-- > 1);
-		
-		i = OPERATION_TIMES;
-		ClearSimpleQueue(p_big_queue);
-		
-	}
-	
-	
-	
-	
-	
-};
-
-TEST(queue_test,mul_entry)
-{
-	uint16_t i = 10000;
-	uint16_t buf_number1,buf_number2,buf_number3,buf_number4,buf_number5;
-	SIMPLE_LOOPQUEUE_DEF(p_queue_1,BIG_QUEUE_SIZE);
-	SIMPLE_LOOPQUEUE_DEF(p_queue_2,BIG_QUEUE_SIZE/2);
-	SIMPLE_LOOPQUEUE_DEF(p_queue_3,BIG_QUEUE_SIZE/4);
-	SIMPLE_LOOPQUEUE_DEF(p_queue_4,BIG_QUEUE_SIZE -255);
-	SIMPLE_LOOPQUEUE_DEF(p_queue_5,BIG_QUEUE_SIZE * 2);
-	
-	do{
-	buf_number1 = SimpleQueueFirstIn(p_queue_1,X_False);
-	
-	buf_number2 = SimpleQueueFirstIn(p_queue_2,X_True);
-	buf_number2 = SimpleQueueFirstIn(p_queue_2,X_True);
-	
-	buf_number5 = SimpleQueueFirstIn(p_queue_5,X_False);
-	buf_number5 = SimpleQueueFirstIn(p_queue_5,X_False);
-	buf_number5 = SimpleQueueFirstIn(p_queue_5,X_False);
-	
-	}while(i-- > 1);
-	
-	
-	EXPECT_EQ(buf_number1,9999);
-	EXPECT_EQ(buf_number2,4999);
-	EXPECT_EQ(buf_number5,INVALID_NODE_NUM);
-	
-	EXPECT_EQ(10000,GetSimpleQueueUsedNodeNumber(p_queue_1));
-	EXPECT_EQ(5000,GetSimpleQueueUsedNodeNumber(p_queue_2));
-	EXPECT_EQ(20000,GetSimpleQueueUsedNodeNumber(p_queue_5));
-	EXPECT_EQ(0,GetSimpleQueueUsedNodeNumber(p_queue_4));
-	
-	buf_number5 = SimpleQueueFirstOut(p_queue_5);
-	EXPECT_EQ(19999,GetSimpleQueueUsedNodeNumber(p_queue_5));
-	EXPECT_EQ(buf_number5,0);
-	
-	UNUSED_VARIABLE(p_queue_3);
-	UNUSED_VARIABLE(buf_number3);
-	UNUSED_VARIABLE(buf_number4);
-};
-
 
 GTEST_API_ int main(int argc, char **argv) {
   cout<<"Running main() from loopqueue_test.cc\n";
