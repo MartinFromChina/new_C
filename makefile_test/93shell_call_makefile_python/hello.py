ef8474615bab15b68f500469609cab9e1f867e68
qbb <qbb@LAPTOP-6A0LGM1A>
2021-01-10 18:12:56 +0800

    python hello.py draw a diagram

---
+++ makefile_test/93shell_call_makefile_python/hello.py
@@ -0,0 +1,18 @@
+
+import matplotlib.pyplot as plt
+filename = 'test.txt'
+X,Y = [],[]
+print("hello world")
+with open(filename, 'r') as f:#1
+    lines = f.readlines()#2
+    for line in lines:#3
+        value = [float(s) for s in line.split()]#4
+        X.append(value[0])#5
+        Y.append(value[1])
+
+print(X)
+print(Y)
+
+plt.plot(X, Y)
+plt.show()
+
