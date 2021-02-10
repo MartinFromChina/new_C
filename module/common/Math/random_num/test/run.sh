94e7a4625598948296bdecd3dc7dba08ea01f43a
qbb <qbb@LAPTOP-6A0LGM1A>
2021-02-11 02:26:10 +0800

    shell passed param successed

--- module/common/Math/random_num/test/run.sh
+++ module/common/Math/random_num/test/run.sh
@@ -1,5 +1,25 @@
+echo "the first param is ": $1
+if [ $# == 0 ]; then
+echo "please enter param such as uniform ; normal"
+exit
+fi
+
+command=$1
 rm random
+
+case $command in
+(uniform)
 make -f uniform_makefile
+;;
+(normal)
+make -f nothing
+;;
+(*)
+echo "wrong param "
+make -f nothing
+;;
+esac
+
 if [ $? -ne "0" ]; then
 echo "make failed!!! please Check error"
 exit
