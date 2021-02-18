c89fdc066bc5de55ec8a2889d34ad5b5540f680c
qbb <qbb@LAPTOP-6A0LGM1A>
2021-02-18 22:04:47 +0800

    get pentip pressure sigema successed

--- tdd_test/work_before/pentip_pressure/pentip_pressure.c
+++ tdd_test/work_before/pentip_pressure/pentip_pressure.c
@@ -10,8 +10,8 @@ X_Void WaitAdc(uint32_t wait_cycle)
 
 X_Boolean PentipInit(GetRawPressure p_get_raw)
 {
-	uint32_t sum;
-	uint16_t pressure,i;
+	uint32_t sum,sum_square;
+	uint16_t pressure,i,temp[SAMPLE_CNT],mean;
 
 	if(p_get_raw == X_Null) {return X_False;}
 	sum = 0;
@@ -20,11 +20,20 @@ X_Boolean PentipInit(GetRawPressure p_get_raw)
 		pressure = p_get_raw();
 		WaitAdc(100);
 		sum += pressure;
-		
-		insert_LogDebug(1,("pressure is %d ;sum  is %d\r\n",pressure,sum ));
-		
+		temp[i] = pressure ;
+		////////////insert_LogDebug(1,("pressure is %d ;sum  is %d\r\n",pressure,sum ));
 	}
 	insert_LogDebug(1,("pressure mean is %d \r\n",sum/SAMPLE_CNT));
+
+	sum_square = 0;
+	mean = sum/SAMPLE_CNT;
+	for(i=0;i<SAMPLE_CNT;i++)
+	{
+		sum_square += (temp[i] - mean) * (temp[i] - mean);
+	}
+	insert_LogDebug(1,("pressure sigema square  is %d \r\n",sum_square/SAMPLE_CNT));
 	
 	isInit = X_True;
+
+	return X_True;
 }
