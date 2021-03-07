95821edab73aa2119cdd4f982f339eb2ec8ee907
qbb <qbb@LAPTOP-6A0LGM1A>
2021-03-07 23:49:27 +0800

    data format convert successed

--- tdd_test/tiny_dancer/6d_motion/py_display/wave_display/wave_display.py
+++ tdd_test/tiny_dancer/6d_motion/py_display/wave_display/wave_display.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 import pylink 
 import sched,time
+import struct
+import math 
+
 
 jlink = pylink.JLink()
 jlink.open()
@@ -11,7 +14,7 @@ print(jlink.device_family())
 print(jlink.core_name())
 isOK = jlink.target_connected()
 
-successed_time = 100
+successed_time = 1000
 ref = 0
 ref_backup = 0
 
@@ -21,7 +24,24 @@ while isOK == True:
         ref = list[0]
         if(ref_backup != ref):
           ref_backup = ref
-          print(list)
+          #print(list)
+          FLOAT32 = []
+          for i in range(0,4,1):FLOAT32.append(list[4-i])
+          #print(FLOAT32)
+          acce_x = struct.unpack('<f', struct.pack('4B', *FLOAT32))[0] # B means unsigned byte
+          print(acce_x)
+
+          FLOAT321 = []
+          for i in range(0,4,1):FLOAT321.append(list[8-i])
+          acce_y = struct.unpack('<f', struct.pack('4B', *FLOAT321))[0]
+          print(acce_y)
+
+          FLOAT322 = []
+          for i in range(0,4,1):FLOAT322.append(list[12-i])
+          acce_z = struct.unpack('<f', struct.pack('4B', *FLOAT322))[0]
+          print(acce_z)
+
+          print('---------------------------------')
           if(successed_time > 0):
             successed_time = successed_time - 1
           else :
