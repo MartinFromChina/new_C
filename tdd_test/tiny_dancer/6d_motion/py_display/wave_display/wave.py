c1f4c8cd68f9290d85eed282b2542636b16e3335
qbb <qbb@LAPTOP-6A0LGM1A>
2021-03-08 22:27:26 +0800

    wave move successed

--- tdd_test/tiny_dancer/6d_motion/py_display/wave_display/wave.py
+++ tdd_test/tiny_dancer/6d_motion/py_display/wave_display/wave.py
@@ -1,5 +1,10 @@
+# encoding: utf-8
 
-
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+from matplotlib import animation
+import numpy as np
+  
 
 def WaveDispaly(p_link,jlink_read):
     sixD_data = []
@@ -11,4 +16,32 @@ def WaveDispaly(p_link,jlink_read):
          print(sixD_data[3])
          print(sixD_data[4])
          print(sixD_data[5])
-    return True
+
+    fig = plt.figure(figsize=(12, 12))
+    ax1 = fig.add_subplot(3,1,1)
+    ax2 = fig.add_subplot(3,1,2)
+    
+    x1 = np.arange(0,1000,1) 
+    y1 = x1 
+
+    x2 = np.arange(0,500,1)  
+    y2 = -x2
+
+    line1,= ax1.plot(x1,y1)
+    line2,= ax2.plot(x2,y2)
+    
+    plt.grid()
+
+    def animate(i):
+       line1.set_ydata(y1 + (i * 200))
+       line2.set_ydata(y2 + (i * 200))
+       return line1,line2,
+
+    def init():
+        line1.set_ydata(0)
+        line2.set_ydata(0)
+        return line1,line2, 
+        
+    ani = animation.FuncAnimation(fig = fig,func=animate,frames = 100,init_func=init,interval = 500,blit = True)
+    
+    plt.show()
--- tdd_test/tiny_dancer/6d_motion/py_display/wave_display/wave_display.py
+++ tdd_test/tiny_dancer/6d_motion/py_display/wave_display/wave_display.py
@@ -1,28 +1,12 @@
 # encoding: utf-8
-import pylink 
 import sched,time
-import struct
 import math 
 
 from jlink import JlinkInit,JlinkClose,JlinkRead
 from wave  import WaveDispaly
 
-isOK = True
-successed_time = 10000
-
 jlink = JlinkInit()
-
-while isOK == True:
-    try:
-         isOK = WaveDispaly(jlink,JlinkRead)
-         if(successed_time > 0):
-            successed_time = successed_time - 1
-         else :
-            break
-    except pylink.errors.JLinkRTTException:
-        time.sleep(0.1)
-
-
+WaveDispaly(jlink,JlinkRead)
 JlinkClose(jlink)
 
 
