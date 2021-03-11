beaed46346e835e59a91a4aed44b916f76b05d83
qbb <qbb@LAPTOP-6A0LGM1A>
2021-03-11 12:44:21 +0800

    draw a 3D picture which can rotate in all three direction

--- tdd_test/tiny_dancer/6d_motion/py_display/wave_display/other.py
+++ tdd_test/tiny_dancer/6d_motion/py_display/wave_display/other.py
@@ -6,26 +6,42 @@ import numpy as np
 from scipy import array 
 from mpl_toolkits.mplot3d import Axes3D
 
-def rotate(angle): 
-    #print(angle)
-    #ax.view_init(angle,45) 
-    a = 0
+surf = None
 
 jet = plt.get_cmap('jet') 
 from matplotlib import animation 
 fig = plt.figure() 
 ax = fig.gca(projection='3d') 
 
-X = np.linspace(70,40,4) 
-Y = np.linspace(5,2,4) 
+X = np.linspace(0,20,20) 
+Y = np.linspace(0,20,20) 
 X,Y= np.meshgrid(X, Y) 
 
+
+'''
 Z = array ([ 
      [1223.539555, 1428.075086,1714.479425, 2144.053223], 
      [1567.26647,1829.056119,2990.416079,2745.320067], 
      [2135.163957,2491.534201, 2990.416079,3738.761638], 
      [3257.280827, 3800.655101, 4561.372117, 5702.458776], 
      ]) 
+'''
+Z = X*X + Y*Y
+def rotate(angle): 
+    #print(angle)
+    ax.view_init(angle,angle) 
+    global surf
+    ax.clear()
+    #Z[0] = Z[0] + angle
+    if(((angle /2) % 2) == 0):
+       Z = X
+       #print(1)
+    else:
+       Z = 4*X + 3*Y
+       #print(0)
+    
+    surf = ax.plot_surface(X, Y, Z, rstride = 1, cstride = 1, cmap = jet,linewidth = 0,alpha= 1) 
+    
 
 surf = ax.plot_surface(X, Y, Z, rstride = 1, cstride = 1, cmap = jet,linewidth = 0,alpha= 1) 
 ax.set_zlim3d(0, Z.max()) 
@@ -35,7 +51,7 @@ ax.set_xlabel('Axial Length [mm]')
 ax.set_ylabel('nbTurns') 
 ax.set_zlabel('RPM') 
 
-rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0,362,2),interval=1000) 
+rot_animation = animation.FuncAnimation(fig, rotate, frames=np.arange(0,362,2),interval=500) 
 
 
 plt.show() 
